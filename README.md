66c188c168f87b88e4e1217ae4c85754c170f2d5
Alexander <leks14@mail.ru>
2023-11-18 18:23:46 +0700

    build 1.0.7

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # CS2-ChatSpy
   The plugin catches the enemy team's message and sends them to the administrators with the @css/chat flag
+
+# Installation
+1. Install [CounterStrike Sharp](https://github.com/roflmuffin/CounterStrikeSharp) and [Metamod:Source](https://www.sourcemm.net/downloads.php/?branch=master)
+3. Download [CS2-ChatSpy](https://github.com/skaen/CS2-ChatSpy/releases)
+4. Unzip the archive and upload it to the game server
+
+# Config
+Grant `@css/chat`` rights to Administrators who will see messages in the config:
+```addons/counterstrikesharp/configs/admins.json```
---
+++ Source/ChatSpy.cs
@@ -0,0 +1,52 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Admin;
+using CounterStrikeSharp.API.Modules.Entities;
+using CounterStrikeSharp.API.Modules.Utils;
+
+namespace ChatSpy;
+
+public class ChatSpy : BasePlugin
+{
+    public override string ModuleName => "Chat Spy";
+    public override string ModuleVersion => "1.0.7";
+    public override string ModuleAuthor => "skaen";
+    public override void Load(bool hotReload)
+    {
+        RegisterEventHandler<EventPlayerChat>(((@event, info) =>
+        {
+            var message = @event.Text.Trim();
+            var player = Utilities.GetPlayerFromUserid(@event.Userid);
+
+            if (player == null || !player.IsValid || player.IsBot || message == null)
+                return HookResult.Continue;
+
+            if (@event.Teamonly)
+            {
+                var isPlayerAlive = player.PawnIsAlive ? "" : "*DEAD*";
+                var team = (CsTeam)player.TeamNum switch
+                {
+                    CsTeam.Terrorist => "Terrorist",
+                    CsTeam.CounterTerrorist => "Counter-Terrorist",
+                    CsTeam.Spectator => "SPEC",
+                    _ => "NONE"
+                };
+
+                var playerEntities = Utilities.GetPlayers();
+                foreach (var admin in playerEntities)
+                {
+                    if (AdminManager.PlayerHasPermissions(admin, "@css/chat"))
+                    {
+                        if (player == admin) continue;
+                        if (player.TeamNum != admin.TeamNum)
+                        {
+                            admin.PrintToChat($" {ChatColors.Grey}{isPlayerAlive} ({team}) {player.PlayerName} : {@event.Text}");
+                        }
+                    }
+                }
+                return HookResult.Continue;
+            }
+            return HookResult.Continue;
+        }));
+    }
+}
