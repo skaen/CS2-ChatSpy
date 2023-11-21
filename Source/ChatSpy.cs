cdcecab96c3f439221c0cbc55f77e7fbb861a776
skaen <leks14@mail.ru>
2023-11-22 01:15:19 +0700

    add config

--- Source/ChatSpy.cs
+++ Source/ChatSpy.cs
@@ -3,14 +3,33 @@ using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Modules.Admin;
 using CounterStrikeSharp.API.Modules.Entities;
 using CounterStrikeSharp.API.Modules.Utils;
+using System.Text.Json.Serialization;
 
 namespace ChatSpy;
-
-public class ChatSpy : BasePlugin
+public class ConfigGen : BasePluginConfig
+{
+    [JsonPropertyName("OnlySpectators")]
+    public bool OnlySpectators { get; set; } = false;
+    [JsonPropertyName("AdminFlag")]
+    public string AdminFlag { get; set; } = "@css/chat";
+    [JsonPropertyName("ColorT")]
+    public string ColorT { get; set; } = "{OLIVE}";
+    [JsonPropertyName("ColorCT")]
+    public string ColorCT { get; set; } = "{BLUE}";
+    [JsonPropertyName("ColorSPEC")]
+    public string ColorSPEC { get; set; } = "{GRAY}";
+}
+public class ChatSpy : BasePlugin, IPluginConfig<ConfigGen>
 {
     public override string ModuleName => "Chat Spy";
-    public override string ModuleVersion => "1.0.7";
+    public override string ModuleVersion => "1.0.8";
     public override string ModuleAuthor => "skaen";
+    public required ConfigGen Config { get; set; }
+    public void OnConfigParsed(ConfigGen config)
+    {
+        Config = config;
+    }
+
     public override void Load(bool hotReload)
     {
         RegisterEventHandler<EventPlayerChat>(((@event, info) =>
@@ -26,21 +45,23 @@ public class ChatSpy : BasePlugin
                 var isPlayerAlive = player.PawnIsAlive ? "" : "*DEAD*";
                 var team = (CsTeam)player.TeamNum switch
                 {
-                    CsTeam.Terrorist => "Terrorist",
-                    CsTeam.CounterTerrorist => "Counter-Terrorist",
-                    CsTeam.Spectator => "SPEC",
+                    CsTeam.Terrorist => $"{Config.ColorT}(Terrorist)",
+                    CsTeam.CounterTerrorist => $"{Config.ColorCT}(Counter-Terrorist)",
+                    CsTeam.Spectator => $"{Config.ColorSPEC}(SPEC)",
                     _ => "NONE"
                 };
 
                 var playerEntities = Utilities.GetPlayers();
                 foreach (var admin in playerEntities)
                 {
-                    if (AdminManager.PlayerHasPermissions(admin, "@css/chat"))
+                    if (AdminManager.PlayerHasPermissions(admin, Config.AdminFlag))
                     {
                         if (player == admin) continue;
+                        if (Config.OnlySpectators && (CsTeam)player.TeamNum != CsTeam.Spectator) continue;
                         if (player.TeamNum != admin.TeamNum)
                         {
-                            admin.PrintToChat($" {ChatColors.Grey}{isPlayerAlive} ({team}) {player.PlayerName} : {@event.Text}");
+                            var messageTeamColor = ReplaceColorTags(team);
+                            admin.PrintToChat($" {ChatColors.Grey}{isPlayerAlive} {messageTeamColor} {player.PlayerName} : {@event.Text}");
                         }
                     }
                 }
@@ -48,5 +69,23 @@ public class ChatSpy : BasePlugin
             }
             return HookResult.Continue;
         }));
+    }
+    private string ReplaceColorTags(string input)
+    {
+        string[] colorPatterns =
+        {
+                "{DEFAULT}", "{RED}", "{LIGHTPURPLE}", "{GREEN}", "{LIME}", "{LIGHTGREEN}", "{LIGHTRED}", "{GRAY}",
+                "{LIGHTOLIVE}", "{OLIVE}", "{LIGHTBLUE}", "{BLUE}", "{PURPLE}", "{GRAYBLUE}"
+            };
+        string[] colorReplacements =
+        {
+                "\x01", "\x02", "\x03", "\x04", "\x05", "\x06", "\x07", "\x08", "\x09", "\x10", "\x0B", "\x0C", "\x0E",
+                "\x0A"
+            };
+
+        for (var i = 0; i < colorPatterns.Length; i++)
+            input = input.Replace(colorPatterns[i], colorReplacements[i]);
+
+        return input;
     }
 }
